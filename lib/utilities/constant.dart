b08e2e9c80e8c76827d79098a7dd8eb0bda83fd5
Mohd-Mahin <mahinit3016@gmail.com>
2021-01-10 16:39:57 +0530

    First commit

---
+++ lib/utilities/constant.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
