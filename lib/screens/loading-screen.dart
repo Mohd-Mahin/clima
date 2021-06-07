a84d5232b0c06a97a46e80819602f163402c8a0f
Mohd-Mahin <mahinit3016@gmail.com>
2021-06-07 20:38:39 +0530

    final commit

--- lib/screens/loading-screen.dart
+++ lib/screens/loading-screen.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+
+import 'package:clima/screens/location-screen.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const appKey = 'c8a2d4b1568d67565bc39eca3443db80';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,13 +15,30 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    geoLocation();
+  }
+
+  void geoLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+
+    var url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=$appKey&units=metric');
+    Response resp = await get(url);
+    var decodeData = jsonDecode(resp.body);
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(weatherData: decodeData);
+    }));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          child: Text('Get Location'),
-          onPressed: () {},
-        ),
+        child: SpinKitDoubleBounce(color: Colors.white, size: 100),
       ),
     );
   }
